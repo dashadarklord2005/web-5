1b3a2a7f1c939ca3c7b39ee31fa78ce6188fb2b7
dashadarklord2005 <dariaserezhina@gmail.com>
2024-12-19 21:13:19 +0300

    Update main.go

--- projects/pipeline/main.go
+++ projects/pipeline/main.go
@@ -1,7 +1,38 @@
 package main
 
-// реализовать removeDuplicates(in, out chan string)
+import "fmt"
+
+func removeDuplicates(inputStream <-chan string, outputStream chan<- string) {
+	defer close(outputStream) 
+
+	var prevValue string 
+	for value := range inputStream {
+		if value != prevValue { 
+			outputStream <- value 
+			prevValue = value     
+		}
+	}
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции removeDuplicates(in, out chan string)
+	in := make(chan string)
+	out := make(chan string)
+
+	go func() {
+		defer close(in) 
+		in <- "one"
+		in <- "two"
+		in <- "two"
+		in <- "three"
+		in <- "three"
+		in <- "three"
+		in <- "four"
+		in <- "five"
+	}()
+
+	go removeDuplicates(in, out)
+
+	for value := range out {
+		fmt.Println(value)
+	}
 }
